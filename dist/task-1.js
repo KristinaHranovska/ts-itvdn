00873f855f786341b58328d6e981f4af702f7791
Kristina <kr.hranovska@gmail.com>
2024-04-03 21:57:39 +0300

    task 1 is ready

--- dist/task-1.js
+++ dist/task-1.js
@@ -1,1 +1,47 @@
-console.log("Hello world");
+const defaultPrice = {
+    smallCap: 10,
+    bigCap: 25,
+    filling: {
+        chocolate: 5,
+        caramel: 6,
+        berries: 10,
+        marshmallow: 5,
+    },
+};
+function getIceCreamPrice() {
+    var _a, _b;
+    let totalPrice = 0;
+    const sizeIceCream = (_a = prompt("Який розмір морозива Ви бажаєте big чи small")) === null || _a === void 0 ? void 0 : _a.toLowerCase();
+    switch (sizeIceCream) {
+        case "big":
+            totalPrice += defaultPrice.bigCap;
+            break;
+        case "small":
+            totalPrice += defaultPrice.smallCap;
+            break;
+        default:
+            alert("Нажаль ми отримали невірні данні спробуйте ще раз");
+            return;
+    }
+    const filling = (_b = prompt("Яку начинку Ви бажаєте додати\nChocolate\nCaramel\nPerries")) === null || _b === void 0 ? void 0 : _b.toLowerCase();
+    switch (filling) {
+        case "caramel":
+            totalPrice += defaultPrice.filling.caramel;
+            break;
+        case "berries":
+            totalPrice += defaultPrice.filling.berries;
+            break;
+        case "chocolate":
+            totalPrice += defaultPrice.filling.chocolate;
+            break;
+        default:
+            alert("Нажаль ми отримали невірні данні спробуйте ще раз");
+            return;
+    }
+    const addFilling = confirm("Ви бажаєте додати маршмелоу");
+    addFilling
+        ? alert((totalPrice += defaultPrice.filling.marshmallow))
+        : alert(totalPrice);
+}
+getIceCreamPrice();
+//# sourceMappingURL=task-1.js.map
--- src/task-1.ts
+++ src/task-1.ts
@@ -1,1 +1,68 @@
-console.log("Hello world");
+/*ви можете використати пакет concurrently для запуску кількох команд одночасно в скрипті start. 
+Спершу встановіть цей пакет, якщо ви ще цього не зробили, використовуючи npm:
+npm install concurrently --save-dev
+*/
+
+type Price = {
+  smallCap: number;
+  bigCap: number;
+  filling: {
+    chocolate: number;
+    caramel: number;
+    berries: number;
+    marshmallow: number;
+  };
+};
+const defaultPrice: Price = {
+  smallCap: 10,
+  bigCap: 25,
+  filling: {
+    chocolate: 5,
+    caramel: 6,
+    berries: 10,
+    marshmallow: 5,
+  },
+};
+function getIceCreamPrice(): void {
+  let totalPrice: number = 0;
+  const sizeIceCream = prompt(
+    "Який розмір морозива Ви бажаєте big чи small"
+  )?.toLowerCase();
+
+  switch (sizeIceCream) {
+    case "big":
+      totalPrice += defaultPrice.bigCap;
+      break;
+    case "small":
+      totalPrice += defaultPrice.smallCap;
+      break;
+    default:
+      alert("Нажаль ми отримали невірні данні спробуйте ще раз");
+      return;
+  }
+
+  const filling = prompt(
+    "Яку начинку Ви бажаєте додати\nChocolate\nCaramel\nPerries"
+  )?.toLowerCase();
+
+  switch (filling) {
+    case "caramel":
+      totalPrice += defaultPrice.filling.caramel;
+      break;
+    case "berries":
+      totalPrice += defaultPrice.filling.berries;
+      break;
+    case "chocolate":
+      totalPrice += defaultPrice.filling.chocolate;
+      break;
+    default:
+      alert("Нажаль ми отримали невірні данні спробуйте ще раз");
+      return;
+  }
+
+  const addFilling = confirm("Ви бажаєте додати маршмелоу");
+  addFilling
+    ? alert((totalPrice += defaultPrice.filling.marshmallow))
+    : alert(totalPrice);
+}
+getIceCreamPrice();
